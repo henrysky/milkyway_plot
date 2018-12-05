f4013fa516b348e190961460bcf73b0f44d50d3c
henrysky <henryskyleung@gmail.com>
2018-12-05 15:30:45 -0500

    general improvement

--- mw_plot/__init__.py
+++ mw_plot/__init__.py
@@ -1,4 +1,7 @@
 from .mw_plot_classes import *
+from pkg_resources import get_distribution
+
+version = __version__ = get_distribution('mw_plot').version
 
 center_coord = [266.4167, -29.0078]
 anti_center_coord = [86.4167, 28.0078]
