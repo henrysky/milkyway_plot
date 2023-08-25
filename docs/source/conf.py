9b58ffea1dc7fba1cbac345e4558dcad1bf9a2e0
Henry Leung <henryskyleung@gmail.com>
2023-08-25 14:35:46 -0400

    update sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,9 +142,6 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
-
 html_show_sourcelink = True
 
 # inheritance diagram
